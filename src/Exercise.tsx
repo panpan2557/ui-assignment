bfb3f462c7483d8d55f061030d3a48c6981b325c
Punpikorn Rattanawirojkul <punpikorn.r@gmail.com>
2024-07-06 18:19:28 +0700

    feat: :sparkles: support committed answer state

--- src/Exercise.tsx
+++ src/Exercise.tsx
@@ -6,17 +6,40 @@ interface ExerciseProps {
   onClickSubmit: () => void;
 }
 
-// function Choice() => {
-// 	return (
-// 		<div className="border hover:border-[#A8BBF0] rounded-2xl py-4 px-6 text-left mb-3 last:mb-0 hover:bg-[#E9EEFB] hover:text-[#1F46B1]">
-//             {choice}
-//           </div>;
-// 	)
-// }
+interface CommittedAnswer {
+  questionId: number;
+  answerId?: number;
+}
+
+function replaceAt<T>(array: Array<T>, index: number, value: T) {
+  const ret = array.slice(0);
+  ret[index] = value;
+  return ret;
+}
 
 export default function Exercise({ questions, onClickSubmit }: ExerciseProps) {
+  const initCommittedAnswers = (questions: Question[]): CommittedAnswer[] => {
+    return questions.map((q) => {
+      return { questionId: q.id, answerId: undefined };
+    });
+  };
+
   const totalQuestions = questions.length;
   const [currentQuestion, setCurrentQuestion] = useState<number>(0);
+  const [selectedChoiceId, setSelectedChoiceId] = useState<number | undefined>(
+    undefined,
+  );
+  const [committedAnswers, setCommittedAnswers] = useState<CommittedAnswer[]>(
+    initCommittedAnswers(questions),
+  );
+  const saveCommittedAnswer = (choiceId: number | undefined) => {
+    setCommittedAnswers(
+      replaceAt(committedAnswers, currentQuestion, {
+        ...committedAnswers[currentQuestion],
+        answerId: choiceId,
+      }),
+    );
+  };
 
   return (
     <div className="flex flex-col p-12 text-left">
@@ -27,7 +50,18 @@ export default function Exercise({ questions, onClickSubmit }: ExerciseProps) {
       <div className="font-normal mb-6">{questions[currentQuestion].text}</div>
       <div className="exercise-choices">
         {questions[currentQuestion].choices.map((choice) => (
-          <div className="border hover:border-[#A8BBF0] rounded-2xl py-4 px-6 text-left mb-3 last:mb-0 hover:bg-[#E9EEFB] hover:text-[#1F46B1]">
+          <div
+            key={`${questions[currentQuestion].id}-${choice.id}`}
+            className={
+              "rounded-2xl py-4 px-6 text-left mb-3 last:mb-0 " +
+              (choice.id === selectedChoiceId
+                ? "border-2 border-[#A8BBF0] bg-[#E9EEFB] text-[#1F46B1] font-bold"
+                : "border hover:border-[#A8BBF0] hover:bg-[#E9EEFB] hover:text-[#1F46B1]")
+            }
+            onClick={() => {
+              setSelectedChoiceId(choice.id);
+            }}
+          >
             {choice.text}
           </div>
         ))}
@@ -41,7 +75,11 @@ export default function Exercise({ questions, onClickSubmit }: ExerciseProps) {
         {currentQuestion > 0 ? (
           <button
             className="rounded-2xl px-8 py-2 border border-[#C7CBD1] text-[#5B6471] "
-            onClick={() => setCurrentQuestion(currentQuestion - 1)}
+            onClick={() => {
+              const targetQuestion = currentQuestion - 1;
+              setCurrentQuestion(targetQuestion);
+              setSelectedChoiceId(committedAnswers[targetQuestion].answerId);
+            }}
           >
             Back
           </button>
@@ -56,7 +94,12 @@ export default function Exercise({ questions, onClickSubmit }: ExerciseProps) {
         ) : (
           <button
             className="rounded-2xl px-8 py-2 bg-[#1F46B1] text-white"
-            onClick={() => setCurrentQuestion(currentQuestion + 1)}
+            onClick={() => {
+              const targetQuestion = currentQuestion + 1;
+              setCurrentQuestion(targetQuestion);
+              saveCommittedAnswer(selectedChoiceId);
+              setSelectedChoiceId(committedAnswers[targetQuestion].answerId);
+            }}
           >
             Next
           </button>
